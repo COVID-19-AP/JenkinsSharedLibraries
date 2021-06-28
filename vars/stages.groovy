9b97ad6c4bf686c83ad24fa1c4c125d204ca096e
Alapakula Purushotham <61377776+apurushotham19@users.noreply.github.com>
2021-06-28 20:52:10 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -1,15 +1,23 @@
-def call(String stageName){
-  
-  if ("${stageName}" == "Build")
-     {
-       sh "mvn clean package"
-     }
-  else if ("${stageName}" == "SonarQube Report")
-     {
-       sh "mvn clean sonar:sonar"
-     }
-  else if ("${stageName}" == "Upload Into Nexus")
-     {
-       sh "mvn clean deploy"
-     }
+def call(string stageName){
+	if ("stringName" == "Build")
+		sh "mvn clean package"
+	else if("stringName" == "SonarQube Report")
+		sh "mvn clean sonar:sonar"
+	else if ("stringName" == "UploadArtifactIntoNexus")
+		sh "mvm clean deploy"
+	else if ("stringName" == "post-success")
+		emailext body: '''Dear Team,
+
+	This build is over and Completed succes!!!!!
+	
+	Best Regards
+	purushotham A
+	9494412397''', subject: 'Build is over !!!', to: 'apurushotham19@gmail.com'
+	else if ("stringName" == "post-failure")
+    
+	This build is over and Completed failure!!!!!
+	
+	Best Regards
+	purushotham A
+	9494412397''', subject: 'Build is over !!!', to: 'apurushotham19@gmail.com'
 }
