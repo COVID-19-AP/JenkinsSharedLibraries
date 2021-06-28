a39e56d166410c9b71ab533777c474582ae7fe37
Alapakula Purushotham <61377776+apurushotham19@users.noreply.github.com>
2021-06-28 21:13:05 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -5,6 +5,10 @@ def call(string stageName){
 		sh "mvn clean sonar:sonar"
 	else if ("stringName" == "UploadArtifactIntoNexus")
 		sh "mvm clean deploy"
+	else if ("stringName" == "DeployIntoServer")
+		sshagent(['3aeb95c5-254c-4c96-b3b6-0fffd385ece8']) {
+		sh "scp -o StrictHostKeyChecking=no target/maven-web-application.war ec2-user@35.154.213.248:/opt/apache-tomcat-9.0.46/webapps"
+	}
 	else if ("stringName" == "post-success")
 		emailext body: '''Dear Team,
 
@@ -13,8 +17,7 @@ def call(string stageName){
 	Best Regards
 	purushotham A
 	9494412397''', subject: 'Build is over !!!', to: 'apurushotham19@gmail.com'
-	else if ("stringName" == "post-failure")
-    
+	else if("stringName" == "post-failure")
 	This build is over and Completed failure!!!!!
 	
 	Best Regards
