ff460ed55a5f1b3893889be6ea0d7d6e6c8d2a7f
Alapakula Purushotham <61377776+apurushotham19@users.noreply.github.com>
2021-06-29 14:31:40 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -1,26 +1,43 @@
 def call(string stageName){
-	if ("stringName" == "Build")
-		sh "mvn clean package"
-	else if("stringName" == "SonarQube Report")
+	if ('stageName' == 'build'){
+		sh "mvn clean build"
+		}
+	else if('stageName' == 'SonarQube report')
+	{
 		sh "mvn clean sonar:sonar"
-	else if ("stringName" == "UploadArtifactIntoNexus")
-		sh "mvm clean deploy"
-	else if ("stringName" == "DeployIntoServer")
-		sshagent(['3aeb95c5-254c-4c96-b3b6-0fffd385ece8']) {
-		sh "scp -o StrictHostKeyChecking=no target/maven-web-application.war ec2-user@35.154.213.248:/opt/apache-tomcat-9.0.46/webapps"
 	}
-	else if ("stringName" == "post-success")
+	else if('stageName' == 'UploadArtifactIntoNexus')
+	{
+		sh "mvn clean deploy"
+	}
+	else if('stageName' == 'DeployIntoTomcat')
+	{
+		sshagent(['45932c08-4117-438a-bfe7-4700629c2b50']) {
+		sh "scp -o StrictHostKeyChecking=no /target/maven-web-application.war ec2-user@65.0.91.17:/opt/apache-tomcat-9.0.46/webapps"
+		}
+	}
+	else if('stageName' == 'success')
+	{
 		emailext body: '''Dear Team,
 
-	This build is over and Completed succes!!!!!
-	
-	Best Regards
-	purushotham A
-	9494412397''', subject: 'Build is over !!!', to: 'apurushotham19@gmail.com'
-	else if("stringName" == "post-failure")
-	This build is over and Completed failure!!!!!
-	
-	Best Regards
-	purushotham A
-	9494412397''', subject: 'Build is over !!!', to: 'apurushotham19@gmail.com'
+		Build is completed using shared library concept using Pipeline Declarative Method is successfully completed.
+
+
+		Best Regatds
+		Purushotham A
+		9494412397
+		''', subject: 'Build is over using Pipeline Declarative Script using shared Libraries', to: 'apurushotham19@gmail.com'
+	}
+	else if('stageName' == 'failure')
+	{
+	emailext body: '''Dear Team,
+
+		Build is completed using shared library concept using Pipeline Declarative Method is failure and completed.
+
+
+		Best Regatds
+		Purushotham A
+		9494412397
+		''', subject: 'Build is over using Pipeline Declarative Script using shared Libraries', to: 'apurushotham19@gmail.com'	
+	}
 }
